7a03aa4663c7b576a2cadc1be11740b4c337ce49
0xFrian <brianfong22@gmail.com>
2022-09-25 01:46:49 -0700

    Commented out everything

--- utils/preview-pdf.js
+++ utils/preview-pdf.js
@@ -1,24 +1,24 @@
-const pdfjs = require("pdfjs-dist");
-
-async function previewPDF(pdf_url) {
-    const loading_task = pdfjs.getDocument(pdf_url);
-    try {
-        const pdf = await loading_task.promise;
-        let pdf_metadata = await pdf.getMetadata();
-
-        pdf_metadata = {
-            title       : pdf_metadata.info.Title ? pdf_metadata.info.Title : "N/A",
-            author      : pdf_metadata.info.Author ? pdf_metadata.info.Author : "N/A",
-            thumbnail: "https://www.freeiconspng.com/uploads/pdf-icon-png-pdf-zum-download-2.png"
-        };
-
-        return pdf_metadata;
-    } catch (error) {
-        throw error;
-    }
-}
-
-module.exports = {
-    previewPDF,
-};
-
+// const pdfjs = require("pdfjs-dist");
+//
+// async function previewPDF(pdf_url) {
+//     const loading_task = pdfjs.getDocument(pdf_url);
+//     try {
+//         const pdf = await loading_task.promise;
+//         let pdf_metadata = await pdf.getMetadata();
+//
+//         pdf_metadata = {
+//             title       : pdf_metadata.info.Title ? pdf_metadata.info.Title : "N/A",
+//             author      : pdf_metadata.info.Author ? pdf_metadata.info.Author : "N/A",
+//             thumbnail: "https://www.freeiconspng.com/uploads/pdf-icon-png-pdf-zum-download-2.png"
+//         };
+//
+//         return pdf_metadata;
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+//
+// module.exports = {
+//     previewPDF,
+// };
+//
