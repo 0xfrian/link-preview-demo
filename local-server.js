6f9e96d8f2719d092bffe43aa98381435902d10d
0xFrian <brianfong22@gmail.com>
2022-09-24 21:57:50 -0700

    Added file for local Express server on localhost:3000 (for local testing)

---
+++ local-server.js
@@ -0,0 +1,43 @@
+const cors = require("cors");
+const express = require("express");
+
+const { previewLink }= require("./utils/preview-link.js");
+const { previewPDF }= require("./utils/preview-pdf.js");
+const { embedTweet }= require("./utils/embed-tweet.js");
+const { getTodaysDate } = require("./utils/helper/get-date.js");
+
+const app = express();
+app.use(express.json());
+app.use(express.static("./public"))
+app.use(cors());
+
+app.post("/api/fetch-metadata", async (req, res) => {
+    const article_url = req.body.article_url.trim().toLowerCase();
+
+    console.log(`Previewing: ${article_url}`);
+    try {
+        let article_metadata = {};
+        if (article_url.includes("https://twitter.com")) {
+            article_metadata = await embedTweet(article_url);
+            article_metadata.type = "tweet";
+        } else if (article_url.includes("pdf")) {
+            article_metadata = await previewPDF(article_url);
+            article_metadata.type = "pdf";
+        } else {
+            article_metadata = await previewLink(article_url, parsed=true);
+            article_metadata.type = "article";
+        }
+        article_metadata.date = getTodaysDate();
+
+        res.send(article_metadata);
+    } catch(error) {
+        console.log("Error: ", error);
+        res.json(error);
+    }
+});
+
+const port = 3000; 
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
+
