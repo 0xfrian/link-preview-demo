d3b5c1005a6bc9234609368a32720a5b5109fa0b
0xFrian <brianfong22@gmail.com>
2022-09-24 22:22:13 -0700

    Updated API path for generating link previews

--- api/index.js
+++ api/index.js
@@ -2,6 +2,11 @@ const path = require("path");
 const cors = require("cors");
 const express = require("express");
 
+const { previewLink } = require("../utils/link-preview.js");
+const { previewPDF }= require("../utils/preview-pdf.js");
+const { embedTweet }= require("../utils/embed-tweet.js");
+const { getTodaysDate } = require("../utils/helper/get-date.js");
+
 const app = express();
 app.use(express.json());
 app.use(express.static("./public"))
@@ -11,8 +16,29 @@ app.get("/api", (_, res) => {
     res.send("Hello! Welcome to the /api page of our Vercel app");
 });
 
-app.post("/api/kek", (req, res) => {
-    res.send(req.body.message + " kek");
+app.post("/api/fetch-metadata", async (req, res) => {
+    const article_url = req.body.article_url.trim().toLowerCase();
+
+    console.log(`Previewing: ${article_url}`);
+    try {
+        let article_metadata = {};
+        if (article_url.includes("https://twitter.com")) {
+            article_metadata = await embedTweet(article_url);
+            article_metadata.type = "tweet";
+        } else if (article_url.includes("pdf")) {
+            article_metadata = await previewPDF(article_url);
+            article_metadata.type = "pdf";
+        } else {
+            article_metadata = await previewLink(article_url, parsed=true);
+            article_metadata.type = "article";
+        }
+        article_metadata.date = getTodaysDate();
+
+        res.send(article_metadata);
+    } catch(error) {
+        console.log("Error: ", error);
+        res.json(error);
+    }
 });
 
 app.all("*", (_, res) => {
