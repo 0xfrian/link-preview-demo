01c7f3e34e37f270fbb24266b98fd397de5bb144
0xFrian <brianfong22@gmail.com>
2022-09-25 01:58:56 -0700

    Removed canvas and pdfjs dependencies; Now applying previewLink() to both Regular and PDF links

--- local-server.js
+++ local-server.js
@@ -2,7 +2,6 @@ const cors = require("cors");
 const express = require("express");
 
 const { previewLink }= require("./utils/preview-link.js");
-const { previewPDF }= require("./utils/preview-pdf.js");
 const { embedTweet }= require("./utils/embed-tweet.js");
 const { getTodaysDate } = require("./utils/helper/get-date.js");
 
@@ -20,9 +19,6 @@ app.post("/api/fetch-metadata", async (req, res) => {
         if (article_url.includes("https://twitter.com")) {
             article_metadata = await embedTweet(article_url);
             article_metadata.type = "tweet";
-        } else if (article_url.slice(-3) === "pdf"){
-            article_metadata = await previewPDF(article_url);
-            article_metadata.type = "pdf";
         } else {
             article_metadata = await previewLink(article_url, parsed=true);
             article_metadata.type = "article";
